55ac183f613ce3c185a821dd6054dd0949d24395
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-08-01 00:45:14 +0500

    + cors + helmet

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const moviesdb = require('mongoose');
 require('dotenv').config();
 const { errors } = require('celebrate');
-// const cors = require('cors');
-// const helmet = require('helmet');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const { isAuthorized } = require('./middlewares/auth');
 const routers = require('./routes/index');
@@ -14,26 +14,26 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-// app.use(helmet());
+app.use(helmet());
 
 moviesdb.connect('mongodb://localhost:27017/bitfilmsdb');
 
-// const allowedCors = [
-//   'http://localhost:3000',
-//   'http://api.diplomalit0s.nomoredomains.xyz/',
-//   'https://api.diplomalit0s.nomoredomains.xyz/',
-//   'api.diplomalit0s.nomoredomains.xyz/',
-// ];
+const allowedCors = [
+  'http://localhost:3000',
+  'http://api.diplomalit0s.nomoredomains.xyz/',
+  'https://api.diplomalit0s.nomoredomains.xyz/',
+  'api.diplomalit0s.nomoredomains.xyz/',
+];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-// app.use(cors({
-//   origin: allowedCors,
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: allowedCors,
+  credentials: true,
+}));
 
 app.use('/', routerAuth);
 
