949600bce224ca353b9fcac954b4252466771466
Aleksey Shemyakin <carl0slit0s@yndex.ru>
2022-07-31 23:59:07 +0500

    fix api

--- app.js
+++ app.js
@@ -1,43 +1,58 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const moviesdb = require('mongoose');
 require('dotenv').config();
+const { errors } = require('celebrate');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const { isAuthorized } = require('./middlewares/auth');
-const routerUsers = require('./routes/users');
-const routerMovies = require('./routes/movies');
+const routers = require('./routes/index');
 const routerAuth = require('./routes/auth');
 const { notFoundPageErorr } = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(helmet());
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+moviesdb.connect('mongodb://localhost:27017/bitfilmsdb');
+
+const allowedCors = [
+  'http://localhost:3000',
+  'http://api.diplomalit0s.nomoredomains.xyz/',
+  'https://api.diplomalit0s.nomoredomains.xyz/',
+  'api.diplomalit0s.nomoredomains.xyz/',
+];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.use(cors({
+  origin: allowedCors,
+  credentials: true,
+}));
+
 app.use('/', routerAuth);
 
 app.use(isAuthorized);
-app.use('/users', routerUsers);
-app.use('/movies', routerMovies);
+app.use('/', routers);
 
 app.use((req, res, next) => {
   next(notFoundPageErorr());
 });
 
 app.use(errorLogger);
+app.use(errors());
 
 app.use((err, req, res, next) => {
-  if (err.statusCode) {
-    return res.status(err.statusCode).send({ message: err.message });
-  }
-
-  return res.status(500).send({ message: 'что-то пошло не так' });
+  const { statusCode, message } = err;
+  res.status(statusCode).send(statusCode === 500
+    ? { message: 'Что-то пошло не так' }
+    : { message });
+  next();
 });
 
 app.listen(PORT);
